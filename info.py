1130c40f9e0e16bdad73d51005709ebe00606f42
Manojthe <114243741+Manojthe@users.noreply.github.com>
2024-09-19 17:37:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 # info.py
 
 # Bot token and other constants
-TOKEN = ''
+TOKEN = '8126488972:AAEccywLM20aahh4w9GEQA_HKDbruvkARJk'
 CAPTION = '''<b>This is Caption boss</b>
 <blockquote>Example HTML blockquote</blockquote>'''
 PIC = 'https://envs.sh/qb0.jpg'

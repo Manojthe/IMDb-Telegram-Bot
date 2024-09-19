55adfb8c231f8cd869e8c71b24815b1713687be3
Manojthe <114243741+Manojthe@users.noreply.github.com>
2024-09-19 17:22:49 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,11 @@
+# info.py
+
+# Bot token and other constants
+TOKEN = ''
+CAPTION = '''<b>This is Caption boss</b>
+<blockquote>Example HTML blockquote</blockquote>'''
+PIC = 'https://envs.sh/qb0.jpg'
+
+# IMDb API details (not directly used in the code, but included for completeness)
+API_ID = 
+API_HASH = ''

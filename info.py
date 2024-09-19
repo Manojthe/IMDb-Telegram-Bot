149781ea760a1fb6a7f89140d577494ccf6040c4
Manojthe <114243741+Manojthe@users.noreply.github.com>
2024-09-19 17:53:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,8 +2,8 @@
 
 # Bot token and other constants
 TOKEN = '8126488972:AAEccywLM20aahh4w9GEQA_HKDbruvkARJk'
-CAPTION = '''<b>This is Caption boss</b>
-<blockquote>Example HTML blockquote</blockquote>'''
+CAPTION = '''<b>🎬 IMDB Bot: Send me the name of any movie or series, and I'll provide you with all the details!</b>
+<blockquote>Mᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : @Bᴏᴛꜱᴜᴘᴘᴏʀᴛꜱ_ᴏɢ</blockquote>'''
 PIC = 'https://envs.sh/qb0.jpg'
 
 # IMDb API details (not directly used in the code, but included for completeness)
